8ad82b325188065b3318386bd41d9fee96d1785a
Chris Kairalla <chris@mailtochris.com>
2014-08-19 20:07:15 -0400

    on plane

--- app.rb
+++ app.rb
@@ -1,91 +1,82 @@
 require 'sinatra'
 require 'twilio-ruby'
-require_relative 'sms.rb'
+require 'redis'
+require 'yaml'
+#require_relative 'sms.rb'
+
 
 #set up sinatra
 configure do
-  set :send_sms_password, "interact2013"
+  set :send_sms_password, ENV["PILL_REMINDER_PASSWORD"]
   conf=YAML.load_file('twilio_conf.yml')
-  set :twilio_account_sid, conf['account_sid']
-  set :twilio_auth_token, conf['auth_token']
-  set :twilio_from_number, conf['from_number']
-
-  mysql_conf=YAML.load_file('mysql_conf.yml')
-  DataMapper.setup(:default, {
-      :adapter => 'mysql',
-      :host => mysql_conf['host'],
-      :username => mysql_conf['username'] ,
-      :password => mysql_conf['password'],
-      :database => mysql_conf['database']})
-  # Automatically create the tables if they don't exist
-  DataMapper.auto_upgrade!
-  # Finish setup
-  DataMapper.finalize
+  set :twilio_account_sid, ENV["TWILIO_SID"] || conf['account_sid']
+  set :twilio_auth_token, ENV["TWILIO_AUTH_TOKEN"] || conf['auth_token']
+  set :twilio_from_number, ENV["TWILIO_NUMBER"] || conf['from_number']
+  set :reminder_receive_number, ENV["PILL_REMINDER_RECEIVE"]
+  set :redis, Redis.new(:host => "127.0.0.1", :port => 6379, :db => 0)
 end
 
 helpers do
-
-  #pick 5 unique numbers 1-59, and 1 number 1-35
-  def get_lotto_numbers
-    nums = []
-    5.times do
-      num = rand(58)+1
-      while nums.include? num
-        num = rand(58)+1
-      end
-      nums << num
-    end
-    nums << rand(34)+1
-    "Your lotto numbers:\n#{nums[0]} #{nums[1]} #{nums[2]} #{nums[3]} #{nums[4]} - Powerball #{nums[5]}\nGood Luck!"
+  def get_msg
+    msg = []
+    msg << "Hi honey, did you take your pills?"
+    msg << "Hey there, just making sure you took your pills.  Let me know."
+    msg << "Pill Time!  Did you take them?"
+    msg << "Ping, Pill time!  Let me know when you've taken them."
+    msg << "Just making sure you took your pills... :)"
+    msg << "Hey sexy!  Take your pills!"
+    msg[rand(msg.size)]
   end
 
-  #get a random cat fact
-  def get_cat_facts
-    facts = []
-    facts << "Cats use their tails to balance and have nearly 30 bones in them!"
-    facts << "In ancient Egypt killing a cat was a crime punishable by death."
-    facts << "Did you know that the first cat show was held in 1871 at the Crystal Palace in London?  Mee-wow!"
-    facts << "Did you know that there are about 100 distinct breeds of domestic cat?  Plenty of furry love!"
-    facts << "Cats bury their feces to cover their trails from predators."
-    facts[rand(facts.size)]
+  def redis
+    settings.redis
+  end
+
+  def get_reply
+    response = []
+    response << "yay!"
+    response << ":)"
+    response << "high five!"
+    response << "Did I mention that you look great today?"
+    response << "You Did It!"
+    response << "You're the best!"
+    response << "Good Job!"
+    response[rand(response.size)]
   end
 end
 
 get "/" do
-  #get the 20 most recent SMS messages
-  @sms_messages = SMS.all(:limit => 20, :order => [:created_at.desc])
-  erb :main
+  ""
 end
 
-#receive SMS and store in database
+#receive SMS and see if it's a positive reply
 post "/receive_sms/?" do
     puts "post receive_sms"
     puts params.inspect
-  body = params["Body"]
-  #send lotto numbers?
-  reply_message = get_lotto_numbers if body.downcase.include? ("lott")
-  #send cat facts?
-  reply_message = get_cat_facts if body.downcase.include?("cat") && body.downcase.include?("facts")
-  if reply_message  # return message
+  body = params["Body"].downcase
+  done = (body.include?("yes") || body.include?("done") || body.include?("took") || body.include?("ok"))
+  if done  # return message
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms reply_message
+      r.Sms get_reply
     end
     twiml.text
-  else #stick message in database
-    sms = SMS.create(:body => params['Body'], :from => params['From'], :to => params['To'], :created_at => Time.now)
+    #todo set msg in Redis
+    redis.set(:took_pills, "true")
   end
-end
-
-
-get "/send_sms/?" do
-   erb :send_sms_form
 end
 
 post "/send_sms/?" do
   puts "post send_sms"
+  today = Time.new.to_date.to_s
+  last_send = redis.get(:last_send)
+  if today != last_send
+    #new day- reset
+    redis.set(:took_pills, "false")
+  end
+  redis.set(:last_send, today)
+  took_pills = (redis.get(:took_pills)=="true") ? true : false
   @message="password is incorrect.  SMS failed." if settings.send_sms_password != params["password"]
-  @message = "missing phone number.  SMS failed." if params["phone_number"].nil? || params["phone_number"].empty?
-  @message = "missing SMS message.  SMS failed." if params["message"].nil? || params["message"].empty?
+  @message="Already took pills today.  Not sending" if took_pills
   #if there's no message then the password matches and we have all of the required info
   if @message.nil?
     puts "sending sms"
@@ -94,10 +85,5 @@ post "/send_sms/?" do
     @sms_reply = account.sms.messages.create({:from => settings.twilio_from_number, :to => params["phone_number"], :body => params["message"]})
     puts "got back this: #{@sms_reply.inspect}"
   end
-
-  if @message #render the original form again, with error message
-    erb :send_sms_form
-  else #render the message that sms was sent
-    erb :sms_sent
-  end
+  @message || "SMS sent"
 end
